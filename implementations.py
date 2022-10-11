8bc1f633e1643d5adb0ece1602be49e8f558af41
francescaventurigit <115399771+francescaventurigit@users.noreply.github.com>
2022-10-11 11:31:28 +0200

    Add files via upload

---
+++ implementations.py
@@ -0,0 +1,425 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import doctest
+import io
+import sys
+
+"""some helper functions.""" # HELPERS
+#%%
+def load_data():
+    """load data."""
+    data = np.loadtxt("dataEx3.csv", delimiter=",", skiprows=1, unpack=True)
+    x = data[0]
+    y = data[1]
+    return x, y
+
+def load_data_fromEx2(sub_sample=True, add_outlier=False):
+    """Load data and convert it to the metric system."""
+    path_dataset = "height_weight_genders.csv"
+    data = np.genfromtxt(
+        path_dataset, delimiter=",", skip_header=1, usecols=[1, 2])
+    height = data[:, 0]
+    weight = data[:, 1]
+    gender = np.genfromtxt(
+        path_dataset, delimiter=",", skip_header=1, usecols=[0],
+        converters={0: lambda x: 0 if b"Male" in x else 1})
+    # Convert to metric system
+    height *= 0.025
+    weight *= 0.454
+
+    # sub-sample
+    if sub_sample:
+        height = height[::50]
+        weight = weight[::50]
+
+    if add_outlier:
+        # outlier experiment
+        height = np.concatenate([height, [1.1, 1.2]])
+        weight = np.concatenate([weight, [51.5/0.454, 55.3/0.454]])
+
+    return height, weight, gender
+
+#%%
+def standardize(x):
+    """Standardize the original data set."""
+    mean_x = np.mean(x)
+    x = x - mean_x
+    std_x = np.std(x)
+    x = x / std_x
+    return x, mean_x, std_x
+
+#%%
+def build_model_data(height, weight):
+    """Form (y,tX) to get regression data in matrix form."""
+    y = weight
+    x = height
+    num_samples = len(y)
+    tx = np.c_[np.ones(num_samples), x]
+    return y, tx
+
+#%%
+def batch_iter(y, tx, batch_size, num_batches=1, shuffle=True):
+    """
+    Generate a minibatch iterator for a dataset.
+    Takes as input two iterables (here the output desired values 'y' and the input data 'tx')
+    Outputs an iterator which gives mini-batches of `batch_size` matching elements from `y` and `tx`.
+    Data can be randomly shuffled to avoid ordering in the original data messing with the randomness of the minibatches.
+    Example of use :
+    for minibatch_y, minibatch_tx in batch_iter(y, tx, 32):
+        <DO-SOMETHING>
+    """
+    data_size = len(y)
+
+    if shuffle:
+        shuffle_indices = np.random.permutation(np.arange(data_size))
+        shuffled_y = y[shuffle_indices]
+        shuffled_tx = tx[shuffle_indices]
+    else:
+        shuffled_y = y
+        shuffled_tx = tx
+    for batch_num in range(num_batches):
+        start_index = batch_num * batch_size
+        end_index = min((batch_num + 1) * batch_size, data_size)
+        if start_index != end_index:
+            yield shuffled_y[start_index:end_index], shuffled_tx[start_index:end_index]
+
+
+
+"""function for plot.""" # PLOTS
+#%%
+def prediction(w0, w1, mean_x, std_x):
+    """Get the regression line from the model."""
+    x = np.arange(1.2, 2, 0.01)
+    x_normalized = (x - mean_x) / std_x
+    return x, w0 + w1 * x_normalized
+
+#%%
+def base_visualization(grid_losses, w0_list, w1_list,
+                       mean_x, std_x, height, weight):
+    """Base Visualization for both models."""
+    w0, w1 = np.meshgrid(w0_list, w1_list)
+
+    fig = plt.figure()
+
+    # plot contourf
+    ax1 = fig.add_subplot(1, 2, 1)
+    cp = ax1.contourf(w0, w1, grid_losses.T, cmap=plt.cm.jet)
+    fig.colorbar(cp, ax=ax1)
+    ax1.set_xlabel(r'$w_0$')
+    ax1.set_ylabel(r'$w_1$')
+    # put a marker at the minimum
+    loss_star, w0_star, w1_star = get_best_parameters(
+        w0_list, w1_list, grid_losses)
+    ax1.plot(w0_star, w1_star, marker='*', color='r', markersize=20)
+
+    # plot f(x)
+    ax2 = fig.add_subplot(1, 2, 2)
+    ax2.scatter(height, weight, marker=".", color='b', s=5)
+    ax2.set_xlabel("x")
+    ax2.set_ylabel("y")
+    ax2.grid()
+
+    return fig
+
+#%%
+def grid_visualization(grid_losses, w0_list, w1_list,
+                       mean_x, std_x, height, weight):
+    """Visualize how the trained model looks like under the grid search."""
+    fig = base_visualization(
+        grid_losses, w0_list, w1_list, mean_x, std_x, height, weight)
+
+    loss_star, w0_star, w1_star = get_best_parameters(
+        w0_list, w1_list, grid_losses)
+    # plot prediciton
+    x, f = prediction(w0_star, w1_star, mean_x, std_x)
+    ax2 = fig.get_axes()[2]
+    ax2.plot(x, f, 'r')
+
+    return fig
+
+#%%
+def gradient_descent_visualization(
+        gradient_losses, gradient_ws,
+        grid_losses, grid_w0, grid_w1,
+        mean_x, std_x, height, weight, n_iter=None):
+    """Visualize how the loss value changes until n_iter."""
+    fig = base_visualization(
+        grid_losses, grid_w0, grid_w1, mean_x, std_x, height, weight)
+
+    ws_to_be_plotted = np.stack(gradient_ws)
+    if n_iter is not None:
+        ws_to_be_plotted = ws_to_be_plotted[:n_iter]
+
+    ax1, ax2 = fig.get_axes()[0], fig.get_axes()[2]
+    ax1.plot(
+        ws_to_be_plotted[:, 0], ws_to_be_plotted[:, 1],
+        marker='o', color='w', markersize=10)
+    pred_x, pred_y = prediction(
+        ws_to_be_plotted[-1, 0], ws_to_be_plotted[-1, 1],
+        mean_x, std_x)
+    ax2.plot(pred_x, pred_y, 'r')
+
+    return fig
+
+#%%
+def plot_fitted_curve(y, x, weights, degree, ax):
+    """plot the fitted curve."""
+    ax.scatter(x, y, color='b', s=12, facecolors='none', edgecolors='r')
+    xvals = np.arange(min(x) - 0.1, max(x) + 0.1, 0.1)
+    tx = build_poly(xvals, degree)
+    f = tx.dot(weights)
+    ax.plot(xvals, f)
+    ax.set_xlabel("x")
+    ax.set_ylabel("y")
+    ax.set_title("Polynomial degree " + str(degree))
+
+#%%
+def plot_train_test(train_errors, test_errors, lambdas, degree):
+    """
+    train_errors, test_errors and lambas should be list (of the same size) the respective train error and test error for a given lambda,
+    * lambda[0] = 1
+    * train_errors[0] = RMSE of a ridge regression on the train set
+    * test_errors[0] = RMSE of the parameter found by ridge regression applied on the test set
+    
+    degree is just used for the title of the plot.
+    """
+    plt.semilogx(lambdas, train_errors, color='b', marker='*', label="Train error")
+    plt.semilogx(lambdas, test_errors, color='r', marker='*', label="Test error")
+    plt.xlabel("lambda")
+    plt.ylabel("RMSE")
+    plt.title("Ridge regression for polynomial degree " + str(degree))
+    leg = plt.legend(loc=1, shadow=True)
+    leg.draw_frame(False)
+    plt.savefig("ridge_regression")
+
+
+#%% 
+def test(f):
+    # The `globs` defines the variables, functions and packages allowed in the docstring.
+    tests = doctest.DocTestFinder().find(f)
+    assert len(tests) <= 1
+    for test in tests:
+        # We redirect stdout to a string, so we can tell if the tests worked out or not
+        orig_stdout = sys.stdout
+        sys.stdout = io.StringIO()
+
+        try:
+            results: doctest.TestResults = doctest.DocTestRunner().run(test)
+            output = sys.stdout.getvalue()
+        finally:
+            sys.stdout = orig_stdout
+
+        if results.failed > 0:
+            print(f"❌ The are some issues with your implementation of `{f.__name__}`:")
+            print(output, end="")
+            print(
+                "**********************************************************************"
+            )
+        elif results.attempted > 0:
+            print(f"✅ Your `{f.__name__}` passed {results.attempted} tests.")
+        else:
+            print(f"Could not find any tests for {f.__name__}")
+
+
+
+
+"""a function used to compute the loss.""" # COSTS
+#%%
+def calculate_mse(e):
+    """Calculate the mse for vector e."""
+    return 1 / 2 * np.mean(e ** 2)
+
+
+#%%
+def calculate_mae(e):
+    """Calculate the mae for vector e."""
+    return np.mean(np.abs(e))
+
+#%%
+def compute_loss(y, tx, w):
+    """Calculate the loss using either MSE or MAE.
+
+    Args:
+        y: shape=(N, )
+        tx: shape=(N,2)
+        w: shape=(2,). The vector of model parameters.
+
+    Returns:
+        the value of the loss (a scalar), corresponding to the input parameters w.
+    """
+    
+    e = y - tx.dot(w)
+    return calculate_mse(e)
+
+
+
+
+"""Grid Search""" # GRID SEARCH
+#%%
+def generate_w(num_intervals):
+    """Generate a grid of values for w0 and w1."""
+    w0 = np.linspace(-100, 200, num_intervals)
+    w1 = np.linspace(-150, 150, num_intervals)
+    return w0, w1
+
+#%%
+def get_best_parameters(w0, w1, losses):
+    """Get the best w from the result of grid search."""
+    min_row, min_col = np.unravel_index(np.argmin(losses), losses.shape)
+    return losses[min_row, min_col], w0[min_row], w1[min_col]
+
+#%%
+def grid_search(y, tx, w0, w1):
+    """Algorithm for grid search."""
+    losses = np.zeros((len(w0), len(w1)))
+    # compute loss for each combination of w0 and w1.
+    for ind_row, row in enumerate(w0):
+        for ind_col, col in enumerate(w1):
+            w = np.array([row, col])
+            losses[ind_row, ind_col] = compute_loss(y, tx, w)
+    return losses
+
+
+
+"""Gradient Descent""" # GRADIENT DESCENT
+#%%
+def compute_gradient(y, tx, w):
+    """Computes the gradient at w.
+
+    Args:
+        y: shape=(N, )
+        tx: shape=(N,2)
+        w: shape=(2, ). The vector of model parameters.
+
+    Returns:
+        An array of shape (2, ) (same shape as w), containing the gradient of the loss at w.
+    """
+   
+    err = y - tx.dot(w)
+    grad = -tx.T.dot(err) / len(err)
+    return grad, err
+
+#%%
+def gradient_descent(y, tx, initial_w, max_iters, gamma):
+    """The Gradient Descent (GD) algorithm.
+
+    Args:
+        y: shape=(N, )
+        tx: shape=(N,2)
+        initial_w: shape=(2, ). The initial guess (or the initialization) for the model parameters
+        max_iters: a scalar denoting the total number of iterations of GD
+        gamma: a scalar denoting the stepsize
+
+    Returns:
+        losses: a list of length max_iters containing the loss value (scalar) for each iteration of GD
+        ws: a list of length max_iters containing the model parameters as numpy arrays of shape (2, ), for each iteration of GD
+    """
+    # Define parameters to store w and loss
+    ws = [initial_w]
+    losses = [compute_loss(y,tx,initial_w)]
+    w = initial_w
+    for n_iter in range(max_iters):
+        # compute loss, gradient
+        grad, err = compute_gradient(y, tx, w)
+        loss = calculate_mse(err)
+        # update w by gradient descent
+        w = w - gamma * grad
+        # store w and loss
+        ws.append(w)
+        losses.append(loss)
+        print("GD iter. {bi}/{ti}: loss={l}, w0={w0}, w1={w1}".format(
+            bi=n_iter, ti=max_iters - 1, l=loss, w0=w[0], w1=w[1]))
+
+    return losses, ws
+
+
+"""Stochastic Gradient Descent""" # STOCHASTIC GRADIENT DESCENT
+#%%
+def compute_stoch_gradient(y, tx, w):
+    """Compute a stochastic gradient at w from just few examples n and their corresponding y_n labels.
+
+    Args:
+        y: shape=(N, )
+        tx: shape=(N,2)
+        w: shape=(2, ). The vector of model parameters.
+
+    Returns:
+        An array of shape (2, ) (same shape as w), containing the stochastic gradient of the loss at w.
+    """
+
+    ### SOLUTION
+    err = y - tx.dot(w)
+    grad = -tx.T.dot(err) / len(err)
+    return grad, err
+
+#%%
+def stochastic_gradient_descent(y, tx, initial_w, batch_size, max_iters, gamma):
+    """The Stochastic Gradient Descent algorithm (SGD).
+
+    Args:
+        y: shape=(N, )
+        tx: shape=(N,2)
+        initial_w: shape=(2, ). The initial guess (or the initialization) for the model parameters
+        batch_size: a scalar denoting the number of data points in a mini-batch used for computing the stochastic gradient
+        max_iters: a scalar denoting the total number of iterations of SGD
+        gamma: a scalar denoting the stepsize
+
+    Returns:
+        losses: a list of length max_iters containing the loss value (scalar) for each iteration of SGD
+        ws: a list of length max_iters containing the model parameters as numpy arrays of shape (2, ), for each iteration of SGD
+    """
+
+    # Define parameters to store w and loss
+    ws = [initial_w]
+    losses = [compute_loss(y, tx, initial_w)]
+    w = initial_w
+
+    for n_iter in range(max_iters):
+
+        for y_batch, tx_batch in batch_iter(y, tx, batch_size=batch_size, num_batches=1):
+            # compute a stochastic gradient and loss
+            grad, _ = compute_stoch_gradient(y_batch, tx_batch, w)
+            # update w through the stochastic gradient update
+            w = w - gamma * grad
+            # calculate loss
+            loss = compute_loss(y, tx, w)
+            # store w and loss
+            ws.append(w)
+            losses.append(loss)
+        
+        print("SGD iter. {bi}/{ti}: loss={l}, w0={w0}, w1={w1}".format(
+            bi=n_iter, ti=max_iters - 1, l=loss, w0=w[0], w1=w[1]))
+    return losses, ws
+
+
+#%%
+def compute_subgradient_mae(y, tx, w):
+    """Compute a subgradient of the MAE at w.
+
+    Args:
+        y: shape=(N, )
+        tx: shape=(N,2)
+        w: shape=(2, ). The vector of model parameters.
+
+    Returns:
+        An array of shape (2, ) (same shape as w), containing the subgradient of the MAE at w.
+    """
+
+    err = y - tx.dot(w)
+    grad = -np.dot(tx.T, np.sign(err)) / len(err)
+    return grad, err
+
+
+"""implement a polynomial basis function."""
+#%%
+def build_poly(x, degree):
+    """polynomial basis functions for input data x, for j=0 up to j=degree."""
+    # ***************************************************
+    phi_x = np.zeros((x.size, degree+1))
+    for i in range(len(x)):
+        curr_row = np.array([x[i]**deg for deg in range(degree+1)])
+        phi_x[i] = curr_row
+        
+    return phi_x
+    # ***************************************************
+    
