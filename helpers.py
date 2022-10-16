777bf92452a5f119d99b6a38b56563039303bab0
francescaventurigit <115399771+francescaventurigit@users.noreply.github.com>
2022-10-16 18:39:36 +0200

    Add files via upload

--- helpers.py
+++ helpers.py
@@ -5,8 +5,8 @@ import io
 import sys
 
 
-#%%
-def load_dataset():
+
+def load_train_dataset():
     """Load data and convert it to the metric system."""
     path_dataset = "train.csv"
     
@@ -24,6 +24,42 @@ def load_dataset():
     data[:, 1] = second_col
     
     return data
+
+#%%
+def load_test_dataset():
+    """Load data and convert it to the metric system."""
+    path_dataset = "test.csv"
+    
+    data = np.genfromtxt(
+            path_dataset, delimiter=",", skip_header=1)
+
+    first_col = np.genfromtxt(
+                path_dataset, delimiter=",", skip_header=1, usecols=[0])
+        
+    second_col = np.genfromtxt(
+                path_dataset, delimiter=",", skip_header=1, usecols=[1],
+                converters={1: lambda x: 0 if b"?" in x else 1})
+    
+    data[:, 0] = first_col
+    data[:, 1] = second_col
+    
+    return data
+
+#%%
+def standardize_train(x):
+    """Standardize the original data set."""
+    ret = x.copy()
+    mean = np.mean(ret[:,2:])
+    std = np.std(ret[:,2:])
+    ret[:,2:] = (ret[:,2:] - mean)/std
+    return ret, mean, std
+#%%
+def standardize_test(x, mean_train, std_train):
+    """Standardize the original data set."""
+    ret = x.copy()
+    ret[:,2:] = (ret[:,2:] - mean_train)/std_train
+    
+    return ret
 
 #%% Count -999 values for each feature
 def count_999_for_feature(dataset):
